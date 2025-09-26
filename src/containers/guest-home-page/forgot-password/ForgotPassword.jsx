143054a0c599ba4df261550f37577bb11d085ff3
Aleksandr Koniev <0719118@gmail.com>
2025-09-26 12:53:28 +0300

    Fix margin bottom to the correct value of 32 px

--- src/containers/guest-home-page/forgot-password/ForgotPassword.jsx
+++ src/containers/guest-home-page/forgot-password/ForgotPassword.jsx
@@ -78,7 +78,7 @@ const ForgotPassword = () => {
 
   return (
     <Box sx={styles.root}>
-      <Box sx={{ mb: 2 }}>
+      <Box sx={{ mb: 4 }}>
         <TitleWithDescription
           description={t('login.enterEmail')}
           style={styles.titleWithDescription}
