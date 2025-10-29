83fe552966f875f5be6fcc014d145a4e445e1f28
kriLEBED <47117380+kriLEBED@users.noreply.github.com>
2025-10-29 16:40:38 +0200

    implement category card and categories list

---
+++ src/components/categories-list/CategoriesList.tsx
@@ -0,0 +1,81 @@
+import { FC, useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
+import Box from '@mui/material/Box'
+
+import CardsList from '~/components/cards-list/CardsList'
+import CardWithLink from '~/components/card-with-link/CardWithLink'
+import NotFoundResults from '~/components/not-found-results/NotFoundResults'
+
+import { useAppSelector } from '~/hooks/use-redux'
+import { authRoutes } from '~/router/constants/authRoutes'
+import { getOpositeRole } from '~/utils/helper-functions'
+import { CategoryInterface } from '~/types'
+
+import { styles } from '~/components/categories-list/CategoriesList.styles'
+
+interface CategoriesListProps {
+  categories: CategoryInterface[]
+  loading: boolean
+  isExpandable: boolean
+  onLoadMore: () => void
+  searchQuery: string
+  onRequestNewCategory: () => void
+}
+
+const CategoriesList: FC<CategoriesListProps> = ({
+  categories,
+  loading,
+  isExpandable,
+  onLoadMore,
+  searchQuery,
+  onRequestNewCategory
+}) => {
+  const { t } = useTranslation()
+  const { userRole } = useAppSelector((state) => state.appMain)
+
+  const oppositeRole = getOpositeRole(userRole)
+
+  const categoryCards = useMemo(
+    () =>
+      categories.map((category: CategoryInterface) => (
+        <CardWithLink
+          key={category._id}
+          img={category.appearance?.icon || ''}
+          title={category.name}
+          description={`${category.totalOffers?.[oppositeRole] || 0} ${t('categoriesPage.offers')}`}
+          link={`${authRoutes.subjects.path}?categoryId=${category._id}`}
+        />
+      )),
+    [categories, oppositeRole, t]
+  )
+
+  if (!categories.length && !loading) {
+    return (
+      <Box sx={styles.container}>
+        <NotFoundResults
+          buttonText={t('errorMessages.buttonRequest', { name: 'categories' })}
+          description={
+            searchQuery
+              ? t('errorMessages.tryAgainText', { name: 'categories' })
+              : t('categoriesPage.description')
+          }
+          onClick={onRequestNewCategory}
+        />
+      </Box>
+    )
+  }
+
+  return (
+    <Box sx={styles.container}>
+      <CardsList
+        cards={categoryCards}
+        btnText={t('categoriesPage.viewMore')}
+        isExpandable={isExpandable}
+        loading={loading}
+        onClick={onLoadMore}
+      />
+    </Box>
+  )
+}
+
+export default CategoriesList
